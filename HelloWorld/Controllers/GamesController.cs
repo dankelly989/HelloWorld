db312de9b288d32483bc9828df523f9cff736466
dan.kelly <dan.kelly@WSAKL001251>
2018-05-10 12:31:18 +1200

    now gets cards from model

--- HelloWorld/Controllers/GamesController.cs
+++ HelloWorld/Controllers/GamesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,8 @@ namespace HelloWorld.Controllers
     {
         public ActionResult Solitaire()
         {
-            return View("Solitaire");
+            var model = new Deck();
+            return View(model);
         }
     }
 }
---
+++ HelloWorld/Models/SolitaireModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class CardModel
+    {
+        public int ID { get; set; }
+        public int value { get; set; }
+        public Suit suit { get; set; }
+
+        public CardModel(int v, Suit s)
+        {
+            value = v;
+            suit = s;
+        }
+    }
+
+    public enum Suit
+    {
+        Club = 0,
+        Heart = 1,
+        Spade = 2,
+        Diamond = 3
+    }
+
+    public class Deck
+    {
+        private List<CardModel> deck;
+        private static Random rng = new Random();
+
+        public Deck()
+        {
+            deck = new List<CardModel>();
+            for (int i = 1; i < 14; i++)
+            {
+                deck.Add(new CardModel(i, Suit.Club));
+                deck.Add(new CardModel(i, Suit.Spade));
+                deck.Add(new CardModel(i, Suit.Heart));
+                deck.Add(new CardModel(i, Suit.Diamond));
+            }
+        }
+        private void shuffle()
+        {
+            int n = deck.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                CardModel c = deck[k];
+                deck[k] = deck[n];
+                deck[n] = c;
+            }
+        }
+        public CardModel getCard()
+        {
+            CardModel c = deck[0];
+            deck.Remove(c);
+            return c;
+        }
+    }
+}
